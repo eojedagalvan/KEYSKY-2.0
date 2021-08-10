47337d11299d063c9d41894a721a9b6aff261d90
eojedagalvan <eduardo.ojeda@edu.uag.mx>
2021-08-10 16:51:32 -0500

    db connection

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("www.keysky.com", "root", "", "KEYSKY");
+$conexion = mysqli_connect("sql140.main-hosting.eu", "u630097666_keysky", "Keysky58", "u630097666_keysky");
 mysqli_set_charset($conexion, "utf8");
 if (!$conexion) {
   echo 'Error al conectar a la base de datos';
