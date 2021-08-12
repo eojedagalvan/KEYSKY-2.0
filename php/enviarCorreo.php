8e7e0fa18ad428e1a20e4d4e01516609d0b6f74a
eojedagalvan <eduardo.ojeda@edu.uag.mx>
2021-08-11 23:59:39 -0500

    Update enviarCorreo.php

--- php/enviarCorreo.php
+++ php/enviarCorreo.php
@@ -66,7 +66,7 @@ try {
 
     $mail->send();
     echo 'Message has been sent';
-    header("Location: ../inicio.php");
+    header("Location: inicio.php");
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
