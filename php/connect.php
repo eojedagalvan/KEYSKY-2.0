d310190ef033524a494abefb9c422f25ef84c03a
Eduardo Ojeda <ojedaeduardo2001@gmail.com>
2023-06-15 19:01:13 -0600

    Update database credential

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("sql140.main-hosting.eu", "u630097666_keysky", "Keysky58", "u630097666_keysky");
+$conexion = mysqli_connect("srv760.hstgr.io", "u630097666_keysky", "Keysky58", "u630097666_keysky");
 mysqli_set_charset($conexion, "utf8");
 if (!$conexion) {
   echo 'Error al conectar a la base de datos';
